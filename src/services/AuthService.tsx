740e0cb7fffe4648b28ea9509740e1df64f28379
Cubaa <jakub29-11@wp.pl>
2021-08-02 22:45:17 +0200

    loluwa

---
+++ src/services/AuthService.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+class AuthService{
+    url = 'http://127.0.0.1:8000/api/';
+
+    async registerUser(userData: any){
+        return axios.post(this.url + 'user/register/', {
+            username: userData.username,
+            last_name: userData.last_name,
+            first_name: userData.first_name,
+            password: userData.password,
+            password2: userData.password2,
+            email: userData.email,
+        }).then( (response:any) => {
+            console.log(response);
+        }).catch( (error:any) => {
+            console.log(error.response.data);
+        });
+    }
+
+    async loginUser(userData: any){
+        return axios.post(this.url + 'user/login/', {
+            username: userData.username,
+            password: userData.password,
+        }).then( (response:any) => {
+            console.log(response);
+        }).catch( (error:any) => {
+            console.log(error);
+        })
+    }
+}
+
+export default new AuthService();
